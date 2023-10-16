75e3868aa7cdfab04ad83ee6549f1ba2fd8cdb55
tardiscover <87391515+tardiscover@users.noreply.github.com>
2023-10-16 17:13:34 -0400

    Changes to ParallaxEffect.cs to shift backgrounds rather than require them to be super wide

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Original from AdamCYounis https://www.youtube.com/watch?v=tMXgLBwtsvI
+// 
+/// <summary>
+/// Adjust the Sprite used and the transform.z (which will affect how "distant" the background is and thus how fast it moves).
+/// The sprite should initially be the width of one complete instance, but set to Draw Mode Tiled.
+/// The could will note this original backgroundWidth, and then multiply the width by 3 so three instance are tiled.
+/// This will allow the view to leave the center instance and then have the whole background seemlessly (and unnoticed) jump from a side instance to the matching position in the center one.
+/// 
+/// Remember to set the Sorting Layer to Background
+/// 
+/// Original from AdamCYounis https://www.youtube.com/watch?v=tMXgLBwtsvI
+/// with modifications based on Chris' Tutorials https://www.youtube.com/watch?v=QHJlXSkwmjo
+/// and Dani https://www.youtube.com/watch?v=zit45k6CUMk
+/// </summary>
 public class ParallaxEffect : MonoBehaviour
 {
     public Camera cam;
@@ -13,6 +25,11 @@ public class ParallaxEffect : MonoBehaviour
 
     //Start Z value of the parallax game object
     float startingZ;
+
+    //The width of the background sprite (just one instance, not counting tiling)
+    private float backgroundWidth;
+
+    private float parallaxEffect;
 
     //Distance that the camera has moved from the starting position of the parallax object
     private Vector2 GetCamMoveSinceStart()
@@ -40,12 +57,27 @@ public class ParallaxEffect : MonoBehaviour
     {
         startingPosition = transform.position;  //Converts Vector3 to Vector2
         startingZ = transform.position.z;
+        backgroundWidth = GetComponent<SpriteRenderer>().bounds.size.x;  //!!!
+        parallaxEffect = GetParallaxFactor();   //!!! Since not changing
+
+        //Stretch the sprite renderer width by 3x.  This results in 3 adjacent tiled images
+        GetComponent<SpriteRenderer>().size = new Vector2(GetComponent<SpriteRenderer>().size.x * 3, GetComponent<SpriteRenderer>().size.y);
     }
 
     private void FixedUpdate()
     {
-        Vector2 newPosition = startingPosition + GetCamMoveSinceStart() * GetParallaxFactor();
+        float temp = cam.transform.position.x * (1.0f - parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
 
-        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);
+        transform.position = new Vector3(startingPosition.x + dist, startingPosition.y, startingZ);
+
+        if (temp > startingPosition.x + backgroundWidth)
+        {
+            startingPosition.x += backgroundWidth;
+        }
+        else if (temp < startingPosition.x - backgroundWidth)
+        {
+            startingPosition.x -= backgroundWidth;
+        }
     }
 }
