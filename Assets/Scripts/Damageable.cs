de9c03f39c5a76802d6940d052f9324a500e7d45
tardiscover <87391515+tardiscover@users.noreply.github.com>
2023-10-30 23:00:45 -0400

    Add a health pickup (not final image?)

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -119,4 +119,18 @@ public class Damageable : MonoBehaviour
         //Unable to be hit
         return false;
     }
+
+    public bool Heal(int healthRestore)
+    {
+        if (IsAlive && Health < MaxHealth)
+        {
+            int maxHeal = Mathf.Max(MaxHealth - Health, 0);
+            int actualHeal = Mathf.Min(maxHeal, healthRestore);
+            Health += actualHeal;
+            CharacterEvents.characterHealed(gameObject, actualHeal);
+            return true;
+        }
+
+        return false;
+    }
 }
