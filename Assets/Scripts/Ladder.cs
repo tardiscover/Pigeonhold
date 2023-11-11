c10ccc31ee45ed73249eca9ade5dc8b2277a36b3
tardiscover <87391515+tardiscover@users.noreply.github.com>
2023-11-11 00:00:47 -0500

    Show the bounds of the ladder box collider temporarily

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -8,6 +8,7 @@ public class Ladder : MonoBehaviour
     public Transform pixel2;
     public Transform pixel3;
     public Transform pixel4;
+    public Transform pixel5;
 
     public RectTransform ladderTop;
     public RectTransform ladderMid;
@@ -15,22 +16,24 @@ public class Ladder : MonoBehaviour
 
     private RectTransform rectTransform;
     private SpriteRenderer ladderMidRenderer;
+    private BoxCollider2D ladderBoxCollider2D;
 
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         ladderMidRenderer = ladderMid.GetComponent<SpriteRenderer>();
+        ladderBoxCollider2D = GetComponent<BoxCollider2D>();
 
-        pixel1.localPosition = new Vector3(pixel1.localPosition.x, 0, pixel1.localPosition.z);
-        //pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderBot.rect.height * 2f, pixel2.localPosition.z);
-        //pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderBot.rect.height * 2f, pixel2.localPosition.z);
-        pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderMid.localPosition.y, pixel2.localPosition.z);
-        //pixel3.localPosition = new Vector3(pixel3.localPosition.x, ladderMid.localPosition.y + ladderMidRenderer.size.y, pixel3.localPosition.z);
-        pixel3.localPosition = new Vector3(pixel3.localPosition.x, ladderTop.localPosition.y - ladderTop.rect.height, pixel3.localPosition.z);
-        pixel4.localPosition = new Vector3(pixel4.localPosition.x, rectTransform.rect.height, pixel4.localPosition.z);
-        //pixel1.position = new Vector3(pixel1.position.x, pixel1.position.y, pixel1.position.z);
-        //pixel4.position = new Vector3(pixel4.position.x, pixel4.position.y, pixel4.position.z);
+        //pixel1.localPosition = new Vector3(pixel1.localPosition.x, 0, pixel1.localPosition.z);
+        ////pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderBot.rect.height * 2f, pixel2.localPosition.z);
+        ////pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderBot.rect.height * 2f, pixel2.localPosition.z);
+        //pixel2.localPosition = new Vector3(pixel2.localPosition.x, ladderMid.localPosition.y, pixel2.localPosition.z);
+        ////pixel3.localPosition = new Vector3(pixel3.localPosition.x, ladderMid.localPosition.y + ladderMidRenderer.size.y, pixel3.localPosition.z);
+        //pixel3.localPosition = new Vector3(pixel3.localPosition.x, ladderTop.localPosition.y - ladderTop.rect.height, pixel3.localPosition.z);
+        //pixel4.localPosition = new Vector3(pixel4.localPosition.x, rectTransform.rect.height, pixel4.localPosition.z);
+        ////pixel1.position = new Vector3(pixel1.position.x, pixel1.position.y, pixel1.position.z);
+        ////pixel4.position = new Vector3(pixel4.position.x, pixel4.position.y, pixel4.position.z);
     }
 
     private void Start()
@@ -46,10 +49,21 @@ public class Ladder : MonoBehaviour
         float btmOfLadderTop = ladderTop.localPosition.y - ladderTop.rect.height;
         float topOfLadderBtm = ladderMid.localPosition.y;
         float newMidOfLadderHeight = btmOfLadderTop - topOfLadderBtm;
+        float newColliderHeight = rectTransform.rect.height - 0.5f;
+        float newColliderYOffset = newColliderHeight / 2.0f;
 
         Debug.Log($"btmOfLadderTop = {btmOfLadderTop}, topOfLadderBtm = {topOfLadderBtm}, newMidOfLadderHeight = {newMidOfLadderHeight}"); //!!!!!!!
         ladderMidRenderer.size = new Vector2(ladderMidRenderer.size.x, newMidOfLadderHeight);    //!!!!!!
-        //ladderMidRenderer.UpdateGIMaterials();
+
+        ladderBoxCollider2D.size = new Vector2(ladderBoxCollider2D.size.x, newColliderHeight);
+        ladderBoxCollider2D.offset = new Vector2(ladderBoxCollider2D.offset.x, newColliderYOffset);
+
+        //Show the bounds of the box collider
+        pixel1.position = new Vector3(ladderBoxCollider2D.bounds.min.x, ladderBoxCollider2D.bounds.max.y, 0f);
+        pixel2.position = new Vector3(ladderBoxCollider2D.bounds.min.x, ladderBoxCollider2D.bounds.min.y, 0f);
+        pixel3.position = new Vector3(ladderBoxCollider2D.bounds.max.x, ladderBoxCollider2D.bounds.min.y, 0f);
+        pixel4.position = new Vector3(ladderBoxCollider2D.bounds.max.x, ladderBoxCollider2D.bounds.max.y, 0f);
+        pixel5.position = ladderBoxCollider2D.bounds.center;
 
     }
 }
